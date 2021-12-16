984e6dbbb747ced659baed68c1d54dfc7208be97
Valentino <69222741+V037@users.noreply.github.com>
2021-12-16 18:15:06 +0100

    Update Pongo.cs

--- Pongo.cs
+++ Pongo.cs
@@ -1,32 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Photon.Pun; //multiplayer
 
 //credits: V.037_
 
 public class Pongo : MonoBehaviour
 {
-    [SerializeField] private Transform pon;
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private float sens = 10;
-    private bool Right;
+    [SerializeField] private Transform pon; //add transform of your stick
+    [SerializeField] private Rigidbody2D rb; //add rigidbody of your stick
+    [SerializeField] private float sens = 10; //sensibility
+    private bool Right; //booleans
     private bool Left;
 
     private Vector2 velo;
-    
-    PhotonView view; //multiplayer
-
-    private void Start()
-    {
-        view = GetComponent<PhotonView>(); //multiplayer
-    }
 
     private void Update()
     {
         if(pon.position.x < 8.5 && Input.GetKey(KeyCode.D))
         {
-            Right = true;
+            Right = true; //more easy way for make this compatible with mobile controls
         }
         else
         {
@@ -45,14 +37,14 @@ public class Pongo : MonoBehaviour
 
     private void FixedUpdate()
     {
-        chec();    
+        chec();    //execute void chec()
     }
 
     private void chec()
     {
         if(Right)
         {
-            velo = new Vector2(1, 0);
+            velo = new Vector2(1, 0); //set direction to right
         }
         if(Left)
         {
@@ -63,6 +55,6 @@ public class Pongo : MonoBehaviour
             velo = new Vector2(0, 0);
         }
 
-        rb.velocity = velo * sens;
+        rb.velocity = velo * sens; //set velocity
     }
 }
